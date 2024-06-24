8c0842087bb942c9c243a23d064f9eef1901835e
vamsi77753 <rocklaro12v@gmail.com>
2024-06-24 10:02:18 +0530

    Sec commit

--- server.js
+++ server.js
@@ -1,17 +1,18 @@
 const express = require("express");
 const path = require("path");
 const app = express();
-const PORT = process.env.PORT || 9000;
 
-// Middleware to serve static files from the 'data' directory
-app.use(express.static(path.join(__dirname, "data")));
-
-// Route to serve questions.json
-app.get("/questions", (req, res) => {
+// Serve the questions.json file
+app.get("/api/questions", (req, res) => {
   res.sendFile(path.join(__dirname, "data", "questions.json"));
 });
 
-// Start the server
-app.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}`);
+// Serve static files from the React app
+app.use(express.static(path.join(__dirname, "build")));
+
+// The "catchall" handler: for any request that doesn't match one above, send back index.html
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "build", "index.html"));
 });
+
+module.exports = app;
